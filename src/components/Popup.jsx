bba6cad00ce01d7cbc159c47b50ddf33898d8528
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-06-17 20:03:22 +0200

    Add files via upload

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Popup = ({ onClose }) => {
   return (
-    <div id="popup" className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
+    <div id="popup" className="fixed inset-0 bg-black bg-opacity-100 flex justify-center items-center z-50">
       <div className="bg-black p-8 rounded shadow-lg text-center">
         <h2 className="text-2xl font-bold mb-4">Weryfikacja uprawnień</h2>
         <p className="mb-4">Witaj, wchodząc na stronę, zgadzasz się na udostępnianie twojego IP do administratora strony</p>
