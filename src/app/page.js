c36db6e5155888240192d37448dd11910c38c19e
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-06-17 17:45:30 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ const App = () => {
     <NextUIProvider>
       <div className="App">
         <Navbar />
-        <VideoBox name="iTVT - Watch Broadcast" src="http://video-itv.itvt.xyz/live/itvt/index.m3u8"/>
+        <VideoBox name="iTVT - Watch Broadcast" src="https://video-itv.itvt.xyz/live/itvt/index.m3u8"/>
         <Channels/>
       </div>
     </NextUIProvider>
