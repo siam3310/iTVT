8ec522bf98f9bcdbc4b896b28a4f064f3e192d00
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-06-18 02:01:59 +0200

    Oliwier stream update

--- src/app/channel/oliwier_stream_local/page.js
+++ src/app/channel/oliwier_stream_local/page.js
@@ -9,7 +9,7 @@ const App = () => {
     <NextUIProvider>
       <div className="App">
         <Navbar />
-        <VideoBox name="Oliwier Stream 24/7" src="https://video-itv.itvt.xyz/live/oliwier_stream/index.m3u8"/>
+        <VideoBox name="Oliwier Stream 24/7" src="http://83.22.152.168:8080/hls/teststrona/index.m3u8"/>
         <Channels/>
       </div>
     </NextUIProvider>
