3d47084b86a5636a6ad1f2d4aa08d3a70948d215
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-15 17:14:54 +0000

    Changed main page popup script

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import App from '../components/App';
-import { cookies } from 'next/headers';
+"use client";
+
+import React, { useEffect, useRef } from 'react';
+import { NextUIProvider } from "@nextui-org/react";
+import FirstLoadPopup from '@/components/FirstLoadPopup';
+import Navbar from '@/components/Navbar';
+import VideoBox from '@/components/VideoBox';
+import Channels from '@/components/Channels';
 
 const Page = () => {
-  const cookieStore = cookies();
-  const hasVisited = cookieStore.has('hasVisited');
+  let appRef = useRef();
+  useEffect(() => {
+    appRef.current.classList.remove("no-clickable", "stop-drag")
+  }, []);
 
-  return <App initialHasVisited={hasVisited} />;
+  return (
+    <NextUIProvider>
+      <FirstLoadPopup />
+      <div className="App no-clickable stop-drag" ref={appRef}>
+        <Navbar />
+        <VideoBox name="iTVT - Watch Broadcast" src="https://video-itv.itvt.xyz/live/itvt/index.m3u8" />
+        <Channels />
+      </div>
+    </NextUIProvider>
+  );
 };
 
 export default Page;
