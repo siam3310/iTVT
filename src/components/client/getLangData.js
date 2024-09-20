37f8dcb9a4f4c143c967ac7a5bfb81a3d84072fa
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-20 23:17:27 +0000

    Improved function to get Lang from user cookie

--- src/components/client/getLangData.js
+++ src/components/client/getLangData.js
@@ -1,5 +1,18 @@
-export const getLangData = () => {
-    const lang = "pl";
+"use client";
+
+import { useCookies } from 'next-client-cookies';
+
+const getLangData = () => {
+    const cookies = useCookies();
+    let lang;
+    switch(cookies.get("hub_lang")){
+      case "pl":
+        lang = "pl";
+        break;
+      default:
+        lang = "en";
+        break;
+    }
     const lang_block = require(`@/json/lang/${lang}.json`);
     try {
         return lang_block;
