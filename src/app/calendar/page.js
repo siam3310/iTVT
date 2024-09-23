03e08c7fa4d8ea65728aab0d355f83bca67ddc70
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 11:35:36 +0200

    Fixed rendering errors 1/1

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -7,23 +7,13 @@ import Navbar from '@/components/Navbar';
 import AccordionBox from '@/components/AccordionBox';
 import Programs from '@/components/client/Programs.js';
 
+
 async function getCalendarText() {
     const lang = await getLangData();
     return lang.pages.tv_calendar;
 }
 
 const App = () => {
-    const programsDemoBroadcast = [
-        { title: "00:00 - 18:00 | Przerwa techniczna", description: "Brak" },
-    ];
-    const programsToday = [
-        { title: "18:00 - 18:30 | Krótkie wprowadzenie", description: "Jakie będą programy | Plan Dnia" },
-        { title: "18:30 - 20:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
-    ];
-    const programsTomorrow = [
-        { title: "10:00 - 10:30 | Program Name", description: "Soon" },
-    ];
-
     const calendarText = use(getCalendarText());
 
     return (
