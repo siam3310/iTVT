2a623cbe2c301ed9056b9d9944afc802c3045065
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 09:36:53 +0200

    Updated TV Guide for 23.09.24

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -13,12 +13,11 @@ async function getCalendarText() {
 
 const App = () => {
     const programsDemoBroadcast = [
-        { title: "01:50 - 09:00 | Czas", description: "Jaka jest teraz godzina" },
-        { title: "10:00 - 19:30 | Przerwa techniczna", description: "Brak" },
+        { title: "00:00 - 18:00 | Przerwa techniczna", description: "Brak" },
     ];
     const programsToday = [
-        { title: "19:30 - 20:00 | Krótkie wprowadzenie", description: "Jakie będą progamy" },
-        { title: "20:00 - 22:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
+        { title: "18:00 - 18:30 | Krótkie wprowadzenie", description: "Jakie będą programy | Plan Dnia" },
+        { title: "18:30 - 20:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
     ];
     const programsTomorrow = [
         { title: "10:00 - 10:30 | Program Name", description: "Soon" },
@@ -33,7 +32,7 @@ const App = () => {
                 <h2 className="text-center font-bold text-3xl mt-2">{calendarText.demo_broadcast}</h2>
                 <AccordionBox programs={programsDemoBroadcast}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.today}</h2>
-                <AccordionBox programs={programsToday}/>
+                <AccordionBox programs={programsToday + " (23.09.2024)"}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.tomorrow}</h2>
                 <AccordionBox programs={programsTomorrow}/>
             </div>
