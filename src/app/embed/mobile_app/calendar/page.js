58a82b22729c8307fd18c46af4a148fa0fafd7b4
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 09:43:33 +0200

    Update TV Guide for 23.09.24

--- src/app/embed/mobile_app/calendar/page.js
+++ src/app/embed/mobile_app/calendar/page.js
@@ -12,12 +12,11 @@ async function getCalendarText() {
 
 const App = () => {
     const programsDemoBroadcast = [
-        { title: "01:50 - 09:00 | Czas", description: "Jaka jest teraz godzina" },
-        { title: "10:00 - 19:30 | Przerwa techniczna", description: "Brak" },
+        { title: "00:00 - 18:00 | Przerwa techniczna", description: "Brak" },
     ];
     const programsToday = [
-        { title: "19:30 - 20:00 | Krótkie wprowadzenie", description: "Jakie będą progamy" },
-        { title: "20:00 - 22:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
+        { title: "18:00 - 18:30 | Krótkie wprowadzenie", description: "Jakie będą programy | Plan Dnia" },
+        { title: "18:30 - 20:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
     ];
     const programsTomorrow = [
         { title: "10:00 - 10:30 | Program Name", description: "Soon" },
