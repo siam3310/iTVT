2499956cfc4fceb8e20ddbce8aad48726fcc2c99
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 11:31:26 +0200

    Moved TV Calendar to external file

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -5,6 +5,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import getLangData from '@/components/client/getLangData';
 import Navbar from '@/components/Navbar';
 import AccordionBox from '@/components/AccordionBox';
+import Programs from '@/components/client/Programs.js';
 
 async function getCalendarText() {
     const lang = await getLangData();
@@ -30,11 +31,11 @@ const App = () => {
             <div className="App">
                 <Navbar />
                 <h2 className="text-center font-bold text-3xl mt-2">{calendarText.demo_broadcast}</h2>
-                <AccordionBox programs={programsDemoBroadcast}/>
+                <AccordionBox programs={Programs.programsDemoBroadcast}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.today}</h2>
-                <AccordionBox programs={programsToday + " (23.09.2024)"}/>
+                <AccordionBox programs={Programs.programsToday + " (23.09.2024)"}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.tomorrow}</h2>
-                <AccordionBox programs={programsTomorrow}/>
+                <AccordionBox programs={Programs.programsTomorrow}/>
             </div>
         </NextUIProvider>
     );
