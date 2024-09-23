278dca52b683fc5200534368acb1e6aa87a58b55
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 10:48:22 +0200

    Moving TV Calendar to external file

--- src/app/embed/mobile_app/calendar/page.js
+++ src/app/embed/mobile_app/calendar/page.js
@@ -4,6 +4,7 @@ import React, {use} from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import getLangData from '@/components/client/getLangData';
 import AccordionBox from '@/components/AccordionBox';
+import Programs from './@components/client/Programs';
 
 async function getCalendarText() {
     const lang = await getLangData();
@@ -11,28 +12,17 @@ async function getCalendarText() {
 }
 
 const App = () => {
-    const programsDemoBroadcast = [
-        { title: "00:00 - 18:00 | Przerwa techniczna", description: "Brak" },
-    ];
-    const programsToday = [
-        { title: "18:00 - 18:30 | Krótkie wprowadzenie", description: "Jakie będą programy | Plan Dnia" },
-        { title: "18:30 - 20:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
-    ];
-    const programsTomorrow = [
-        { title: "10:00 - 10:30 | Program Name", description: "Soon" },
-    ];
-
     const calendarText = use(getCalendarText());
 
     return (
         <NextUIProvider>
             <div className="App">
                 <h2 className="text-center font-bold text-3xl mt-2">{calendarText.demo_broadcast}</h2>
-                <AccordionBox programs={programsDemoBroadcast}/>
+                <AccordionBox programs={Programs.programsDemoBroadcast}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.today}</h2>
-                <AccordionBox programs={programsToday}/>
+                <AccordionBox programs={Programs.programsToday}/>
                 <h2 className="text-center font-bold text-3xl mt-5">{calendarText.tomorrow}</h2>
-                <AccordionBox programs={programsTomorrow}/>
+                <AccordionBox programs={Programs.programsTomorrow}/>
             </div>
         </NextUIProvider>
     );
