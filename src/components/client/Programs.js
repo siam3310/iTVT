7080383238f128926d03fe2f668da9b4441ff2f8
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-09-23 10:45:43 +0200

    Moving TV Calendar to external file

---
+++ src/components/client/Programs.js
@@ -0,0 +1,16 @@
+class Programs {
+  static programsDemoBroadcast = [
+    { title: "00:00 - 18:00 | Przerwa techniczna", description: "Brak" },
+  ];
+
+  static programsToday = [
+    { title: "18:00 - 18:30 | Krótkie wprowadzenie", description: "Jakie będą programy | Plan Dnia" },
+    { title: "18:30 - 20:30 | Zagrajmy na luzie z Klubuntu", description: "Program rozwrywkowy" },
+  ];
+
+  static programsTomorrow = [
+    { title: "10:00 - 10:30 | Program Name", description: "Soon" },
+  ];
+}
+
+export default Programs;
