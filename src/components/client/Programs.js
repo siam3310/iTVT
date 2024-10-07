ce3d5640f4f8e1ac79a5f4f8a820ba8feda9e216
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-10-08 00:52:11 +0200

    Update TV Programs for 08.10.24

--- src/components/client/Programs.js
+++ src/components/client/Programs.js
@@ -1,15 +1,17 @@
 class Programs {
   static programsDemoBroadcast = [
-    { title: "00:00 - 21:00 | Przerwa techniczna", description: "Brak" },
+    { title: "00:00 - 17:35 | Przerwa techniczna", description: "Brak" },
   ];
 
   static programsToday = [
-    { title: "20:55 - 21:00 | Słowem Wstępu", description: "Jakie będą programy | Plan Dnia" },
-    { title: "21:00 - 23:00 | Zagrajmy na luzie z Klubuntu", description: "W drogę po plakaty | Program rozrywkowy" },
+    { title: "17:35 - 17:50 | Oliwier vs Kubix - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
+    { title: "17:50 - 18:03 | Oliwier vs Kamil Nalepa - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
+    { title: "18:03 - 18:25 | Test Mityków Fortnite - Oliwier Design", description: "Rozgrywka w popularnej grze Battle Royale" },
+    { title: "18:25 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - co byś zmienił !?" },
   ];
 
   static programsTomorrow = [
-    { title: "10:00 - 10:30 | Program Name", description: "Soon" },
+    { title: "Brak", description: "Soon" },
   ];
 }
 
