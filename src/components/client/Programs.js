2cfabdfbe16a2613ba4b09956d3013d614676bbd
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-10-08 00:52:53 +0200

    Update TV Guide for 08.10.24

--- src/components/client/Programs.js
+++ src/components/client/Programs.js
@@ -7,7 +7,7 @@ class Programs {
     { title: "17:35 - 17:50 | Oliwier vs Kubix - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
     { title: "17:50 - 18:03 | Oliwier vs Kamil Nalepa - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
     { title: "18:03 - 18:25 | Test Mityków Fortnite - Oliwier Design", description: "Rozgrywka w popularnej grze Battle Royale" },
-    { title: "18:25 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - co byś zmienił !?" },
+    { title: "18:25 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - czy coś byś zmienił ?" },
   ];
 
   static programsTomorrow = [
