80b22eb7302fa855acfe312168bd6ac27dee069d
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-10-08 22:52:24 +0200

    Updated TV Guide for 09.10.24

--- src/components/client/Programs.js
+++ src/components/client/Programs.js
@@ -1,13 +1,17 @@
 class Programs {
   static programsDemoBroadcast = [
-    { title: "00:00 - 17:35 | Przerwa techniczna", description: "Brak" },
+    { title: "00:00 - 09:30 | Przerwa techniczna", description: "Brak" },
   ];
 
   static programsToday = [
-    { title: "17:35 - 17:50 | Oliwier vs Kubix - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
-    { title: "17:50 - 18:03 | Oliwier vs Kamil Nalepa - CS GO", description: "Rozgrywka z znajomymi w popularną grę strzelankową" },
-    { title: "18:03 - 18:25 | Test Mityków Fortnite - Oliwier Design", description: "Rozgrywka w popularnej grze Battle Royale" },
-    { title: "18:25 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - czy coś byś zmienił ?" },
+    { title: "09:28 - 11:48 | Tworzymy własny Bootloader #part2", description: "Zapis transmisji z kanału Klubuntu" },
+    { title: "11:48 - 15:32 | Tworzymy własny Bootloader #part3", description: "Zapis transmisji z kanału Klubuntu" },
+    { title: "15:32 - 16:10 | Prawdziwe życie (S2) #1", description: "GTA RP - Johny" },
+    { title: "15:10 - 16:31 | Prawdziwe życie (S2) #3", description: "GTA RP - Niespodzianka" },
+    { title: "16:31 - 17:03 | Prawdziwe życie (S2) #4", description: "GTA RP - Wakacje" },
+    { title: "17:03 - 17:17 | GTA - Przez całą mapę", description: "Rozgrywka w popularnej grze" },
+    { title: "17:17 - 17:45 | GTA 5 Ścigani - Nowa Przygoda", description: "Odcinek 1 ■ Rozgrywka w popularnej grze" },
+    { title: "17:45 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - czy coś byś zmienił ?" },
   ];
 
   static programsTomorrow = [
