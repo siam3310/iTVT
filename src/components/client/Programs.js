a8dae9be78fe78e5258a38879da5dfcf7defb8a1
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-10-10 15:15:08 +0200

    Update TV Programs for 10.10.24

--- src/components/client/Programs.js
+++ src/components/client/Programs.js
@@ -1,17 +1,10 @@
 class Programs {
   static programsDemoBroadcast = [
-    { title: "00:00 - 09:30 | Przerwa techniczna", description: "Brak" },
+    { title: "00:00 - 18:05 | Przerwa techniczna", description: "Brak" },
   ];
 
   static programsToday = [
-    { title: "09:28 - 11:48 | Tworzymy własny Bootloader #part2", description: "Zapis transmisji z kanału Klubuntu" },
-    { title: "11:48 - 15:32 | Tworzymy własny Bootloader #part3", description: "Zapis transmisji z kanału Klubuntu" },
-    { title: "15:32 - 16:10 | Prawdziwe życie (S2) #1", description: "GTA RP - Johny" },
-    { title: "15:10 - 16:31 | Prawdziwe życie (S2) #3", description: "GTA RP - Niespodzianka" },
-    { title: "16:31 - 17:03 | Prawdziwe życie (S2) #4", description: "GTA RP - Wakacje" },
-    { title: "17:03 - 17:17 | GTA - Przez całą mapę", description: "Rozgrywka w popularnej grze" },
-    { title: "17:17 - 17:45 | GTA 5 Ścigani - Nowa Przygoda", description: "Odcinek 1 ■ Rozgrywka w popularnej grze" },
-    { title: "17:45 - 20:05 | Oliwier Stream - IRL ", description: "Zobacz jak wygląda moje życie - czy coś byś zmienił ?" },
+    { title: "18:05 - 20:15 | Oliwier Stream - IRL (Dzień 3) ", description: "Zobacz jak wygląda moje życie - czy coś byś zmienił ?" },
   ];
 
   static programsTomorrow = [
