8230ddb66ad68199d3b6b1980252679ef6e22962
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-10-11 11:36:03 +0200

    Update TV Guide for Today

--- src/components/client/Programs.js
+++ src/components/client/Programs.js
@@ -1,10 +1,10 @@
 class Programs {
   static programsDemoBroadcast = [
-    { title: "00:00 - 18:05 | Przerwa techniczna", description: "Brak" },
+    { title: "00:00 - 23:59 | Przerwa techniczna", description: "Brak" },
   ];
 
   static programsToday = [
-    { title: "18:05 - 20:15 | Oliwier Stream - IRL (Dzień 4) ", description: "Zobacz jak wygląda moje życie - nie wiem co dzisiaj robić ?" },
+    { title: "Brak", description: "Soon" },
   ];
 
   static programsTomorrow = [
