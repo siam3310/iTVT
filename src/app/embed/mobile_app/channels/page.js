c2d4b2f3beaa6f76446a7e7de71746b5eec2c138
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2025-01-14 13:49:56 +0100

    Removed Privacy Popup for Embed

--- src/app/embed/mobile_app/channels/page.js
+++ src/app/embed/mobile_app/channels/page.js
@@ -3,7 +3,6 @@
 import React, { use, useEffect, useRef } from 'react';
 import {NextUIProvider} from "@nextui-org/react";
 import getLangData from '@/components/client/getLangData';
-import FirstLoadPopup from '@/components/FirstLoadPopup';
 import Channels from '@/components/Channels';
 
 async function getChannelsText(){
@@ -21,7 +20,6 @@ const Page = () => {
 
   return (
     <NextUIProvider>
-      <FirstLoadPopup />
       <div className="App no-clickable stop-drag" ref={appRef}>
         <h1 className='font-bold text-2xl text-center my-7'>{channelsText}</h1>
         <Channels/>
